eff56975f8d294346c0848ebe792efbbb5a6634c
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-03-24 14:32:16 -0300

    Desenvolvendo User Company, reescrevendo AbstractBaseUser

--- tcc_meihelp_backend/core/admin.py
+++ tcc_meihelp_backend/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+import tcc_meihelp_backend.company.models
 
 # Register your models here.
-# admin.site.register(tcc_meihelp_backend.company.models.)
+
+admin.site.register(tcc_meihelp_backend.company.models.Company)
