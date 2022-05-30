f18cacd2a7df255560df4c73f516b72382979b1d
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-05-30 19:01:16 -0300

    Criando Model Training

--- tcc_meihelp_backend/trainings/models.py
+++ tcc_meihelp_backend/trainings/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Training(models.Model):
+    url = models.CharField('URL do vídeo')
+    title = models.CharField('Nome do vídeo', max_length=32)
+    description = models.TextField('Descrição do vídeo')
+    created_at = models.DateTimeField(default=datetime.now(), editable=False)
+    updated_at = models.DateTimeField(default=datetime.now())
+
+    class Meta:
+        verbose_name = 'Training'
+        verbose_name_plural = 'Trainings'
