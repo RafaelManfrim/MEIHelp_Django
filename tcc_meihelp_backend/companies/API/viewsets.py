54f7a32b5869ead0a7ea666f6f01b1ba87b741fa
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-21 17:05:34 -0300

    Removendo importação desnecessária

--- tcc_meihelp_backend/companies/API/viewsets.py
+++ tcc_meihelp_backend/companies/API/viewsets.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from django.db.models import Q
 from rest_framework import viewsets, status
-from rest_framework.decorators import action, permission_classes
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
